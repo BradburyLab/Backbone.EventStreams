5d800cf2e35d7dc127886f147c5dec7e12d75de5
Daniel K <x.flowwolf@gmail.com>
2014-04-14 14:19:08 +0400

    added AMD support

--- dist/backbone.eventstreams.js
+++ dist/backbone.eventstreams.js
@@ -1,79 +1,86 @@
 (function() {
-  var __slice = [].slice;
+  var Backbone, Bacon, init,
+    __slice = [].slice;
 
-  Backbone.EventStream = {
-    listenToEventStream: function(eventTarget, eventName, eventTransformer) {
-      var listener;
-      if (eventTransformer == null) {
-        eventTransformer = _.identity;
+  init = function(Bacon, Backbone) {
+    Backbone.EventStream = {
+      listenToEventStream: function(eventTarget, eventName, eventTransformer) {
+        var listener;
+        if (eventTransformer == null) {
+          eventTransformer = _.identity;
+        }
+        listener = this;
+        return new Bacon.EventStream(function(sink) {
+          var handler, unbind;
+          handler = function() {
+            var args, reply;
+            args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+            reply = sink(new Bacon.Next(eventTransformer.apply(null, args)));
+            if (reply === Bacon.noMore) {
+              return unbind();
+            }
+          };
+          unbind = function() {
+            return listener.stopListening(eventTarget, eventName, handler);
+          };
+          listener.listenTo(eventTarget, eventName, handler);
+          return unbind;
+        });
+      },
+      asEventStream: function(eventName, eventTransformer) {
+        var eventTarget;
+        if (eventTransformer == null) {
+          eventTransformer = _.identity;
+        }
+        eventTarget = this;
+        return new Bacon.EventStream(function(sink) {
+          var handler, unbind;
+          handler = function() {
+            var args, reply;
+            args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+            reply = sink(new Bacon.Next(eventTransformer.apply(null, args)));
+            if (reply === Bacon.noMore) {
+              return unbind();
+            }
+          };
+          unbind = function() {
+            return eventTarget.off(eventName, handler);
+          };
+          eventTarget.on(eventName, handler, this);
+          return unbind;
+        });
       }
-      listener = this;
-      return new Bacon.EventStream(function(sink) {
-        var handler, unbind;
-        handler = function() {
-          var args, reply;
-          args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-          reply = sink(new Bacon.Next(eventTransformer.apply(null, args)));
-          if (reply === Bacon.noMore) {
-            return unbind();
-          }
+    };
+    _.extend(Backbone, Backbone.EventStream);
+    _.extend(Backbone.Model.prototype, Backbone.EventStream);
+    _.extend(Backbone.Collection.prototype, Backbone.EventStream);
+    _.extend(Backbone.Router.prototype, Backbone.EventStream);
+    _.extend(Backbone.History.prototype, Backbone.EventStream);
+    _.extend(Backbone.View.prototype, Backbone.EventStream);
+    Backbone.BaconProperty = {
+      toModel: function() {
+        var handler, model;
+        model = new Backbone.Model(this.take(1));
+        handler = function(value) {
+          return model.set(value);
         };
-        unbind = function() {
-          return listener.stopListening(eventTarget, eventName, handler);
-        };
-        listener.listenTo(eventTarget, eventName, handler);
-        return unbind;
-      });
-    },
-    asEventStream: function(eventName, eventTransformer) {
-      var eventTarget;
-      if (eventTransformer == null) {
-        eventTransformer = _.identity;
+        this.onValue(handler);
+        return model;
       }
-      eventTarget = this;
-      return new Bacon.EventStream(function(sink) {
-        var handler, unbind;
-        handler = function() {
-          var args, reply;
-          args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-          reply = sink(new Bacon.Next(eventTransformer.apply(null, args)));
-          if (reply === Bacon.noMore) {
-            return unbind();
-          }
-        };
-        unbind = function() {
-          return eventTarget.off(eventName, handler);
-        };
-        eventTarget.on(eventName, handler, this);
-        return unbind;
-      });
-    }
+    };
+    return _.extend(Bacon.Property.prototype, Backbone.BaconProperty);
   };
 
-  _.extend(Backbone, Backbone.EventStream);
-
-  _.extend(Backbone.Model.prototype, Backbone.EventStream);
-
-  _.extend(Backbone.Collection.prototype, Backbone.EventStream);
-
-  _.extend(Backbone.Router.prototype, Backbone.EventStream);
-
-  _.extend(Backbone.History.prototype, Backbone.EventStream);
-
-  _.extend(Backbone.View.prototype, Backbone.EventStream);
-
-  Backbone.BaconProperty = {
-    toModel: function() {
-      var handler, model;
-      model = new Backbone.Model(this.take(1));
-      handler = function(value) {
-        return model.set(value);
-      };
-      this.onValue(handler);
-      return model;
+  if (typeof module !== "undefined" && module !== null) {
+    Bacon = require("baconjs");
+    Backbone = require("backbone");
+    module.exports = init(Bacon, Backbone);
+  } else {
+    if (typeof define === "function" && define.amd) {
+      define(["bacon", "backbone"], init);
+    } else {
+      init(this.Bacon, this.Backbone);
     }
-  };
-
-  _.extend(Bacon.Property.prototype, Backbone.BaconProperty);
+  }
 
 }).call(this);
--- src/backbone.eventstreams.coffee
+++ src/backbone.eventstreams.coffee
@@ -1,42 +1,53 @@
-Backbone.EventStream =
-  listenToEventStream: (eventTarget, eventName, eventTransformer = _.identity) ->
-    listener = this
-    new Bacon.EventStream (sink) ->
-      handler = (args...) ->
-        reply = sink(new Bacon.Next(eventTransformer args...))
-        if reply == Bacon.noMore
-          unbind()
+init = (Bacon, Backbone) ->
+  Backbone.EventStream =
+    listenToEventStream: (eventTarget, eventName, eventTransformer = _.identity) ->
+      listener = this
+      new Bacon.EventStream (sink) ->
+        handler = (args...) ->
+          reply = sink(new Bacon.Next(eventTransformer args...))
+          if reply == Bacon.noMore
+            unbind()
 
-      unbind = -> listener.stopListening(eventTarget, eventName, handler)
-      listener.listenTo(eventTarget, eventName, handler)
-      unbind
+        unbind = -> listener.stopListening(eventTarget, eventName, handler)
+        listener.listenTo(eventTarget, eventName, handler)
+        unbind
 
-  asEventStream: (eventName, eventTransformer = _.identity) ->
-    eventTarget = this
-    new Bacon.EventStream (sink) ->
-      handler = (args...) ->
-        reply = sink(new Bacon.Next(eventTransformer args...))
-        if reply == Bacon.noMore
-          unbind()
+    asEventStream: (eventName, eventTransformer = _.identity) ->
+      eventTarget = this
+      new Bacon.EventStream (sink) ->
+        handler = (args...) ->
+          reply = sink(new Bacon.Next(eventTransformer args...))
+          if reply == Bacon.noMore
+            unbind()
 
-      unbind = -> eventTarget.off(eventName, handler)
-      eventTarget.on(eventName, handler, this)
-      unbind
+        unbind = -> eventTarget.off(eventName, handler)
+        eventTarget.on(eventName, handler, this)
+        unbind
 
-_.extend Backbone,                      Backbone.EventStream
-_.extend Backbone.Model.prototype,      Backbone.EventStream
-_.extend Backbone.Collection.prototype, Backbone.EventStream
-_.extend Backbone.Router.prototype,     Backbone.EventStream
-_.extend Backbone.History.prototype,    Backbone.EventStream
-_.extend Backbone.View.prototype,       Backbone.EventStream
+  _.extend Backbone,                      Backbone.EventStream
+  _.extend Backbone.Model.prototype,      Backbone.EventStream
+  _.extend Backbone.Collection.prototype, Backbone.EventStream
+  _.extend Backbone.Router.prototype,     Backbone.EventStream
+  _.extend Backbone.History.prototype,    Backbone.EventStream
+  _.extend Backbone.View.prototype,       Backbone.EventStream
 
-Backbone.BaconProperty =
-  toModel: ->
-    model = new Backbone.Model(this.take(1))
-    handler = (value) ->
-      model.set value
-    this.onValue(handler)
-    model
-      
+  Backbone.BaconProperty =
+    toModel: ->
+      model = new Backbone.Model(this.take(1))
+      handler = (value) ->
+        model.set value
+      this.onValue(handler)
+      model
+        
 
-_.extend Bacon.Property.prototype,      Backbone.BaconProperty
+  _.extend Bacon.Property.prototype,      Backbone.BaconProperty
+
+if module?
+  Bacon = require("baconjs")
+  Backbone = require("backbone")
+  module.exports = init(Bacon, Backbone)
+else
+  if typeof define == "function" and define.amd
+    define ["bacon", "backbone"], init
+  else
+    init(this.Bacon, this.Backbone)
